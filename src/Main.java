04ed1f879e27f39f6592a595a7549da35d5e8eab
puyyy <116352262+reppyassiddiq@users.noreply.github.com>
2024-03-19 20:51:05 +0700

    first init github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello My Name Is Reppy");
+        System.out.println("Im From Class TIF RM 22A");
     }
 }
